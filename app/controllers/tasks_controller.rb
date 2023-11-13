78f785e5d5a78360fd8b76e46e44754821dea1f9
menahil liaqat <zoyagee786@gmail.com>
2023-11-13 17:15:48 +1100

    rails checklist task RL2-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+  before_action 
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @new = Task.new # Needed to instantiate the form_with
+  end
+
+  def create
+
+    @new = Task.new(task_params)
+    @new.save #save the new task and redirect
+    redirect_to tasks_path
+
+  end
+
+  def edit
+    @edit = Task.find(params[:id])
+  end
+
+  def update
+    @edit = Task.find(params[:id])
+    @edit.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @delete = Task.find(params[:id])
+    @delete.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+
+  end
+
 end
