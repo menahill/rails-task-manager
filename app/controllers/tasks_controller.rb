24162bfc2c448da6498057d99055d671d8fed73b
menahil liaqat <zoyagee786@gmail.com>
2023-11-13 18:51:15 +1100

    update design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action 
+  # before_action
   def index
     @tasks = Task.all
   end
@@ -37,6 +37,7 @@ class TasksController < ApplicationController
   end
 
   private
+
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
